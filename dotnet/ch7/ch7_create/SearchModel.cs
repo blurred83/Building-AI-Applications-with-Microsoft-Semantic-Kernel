cea965c77093d7514ebaabdaa93b3561926222e4
Max Szczurek <mszczurek@tamb2cc.com>
2024-12-10 17:17:11 -0600

    Reorganized project/solution names. Now the top level solution loads everything, the chapter solutions load all the projects in the chapter, each project has its own .sln of the same name, and there are no duplicate project names. All projects are prefixed with ch[1-5]_

--- dotnet/ch7/ch7_create/SearchModel.cs
+++ dotnet/ch7/ch7_create/SearchModel.cs
@@ -3,19 +3,19 @@ using Azure.Search.Documents.Indexes.Models;
 public class SearchModel
 {
     [SimpleField(IsKey = true, IsSortable = true, IsFilterable = true, IsFacetable = true)]
-    public string Id { get; set; }
+    public string? Id { get; set; }
 
     [SearchableField]
-    public string AdditionalMetadata { get; set; }
+    public string? AdditionalMetadata { get; set; }
 
     [SearchableField]
-    public string Text { get; set; }
+    public string? Text { get; set; }
 
     [SearchableField]
-    public string Description { get; set; }
+    public string? Description { get; set; }
 
     [SearchableField]
-    public string ExternalSourceName { get; set; }
+    public string? ExternalSourceName { get; set; }
 
     [SimpleField(IsFilterable = true)]
     public bool IsReference { get; set; }
